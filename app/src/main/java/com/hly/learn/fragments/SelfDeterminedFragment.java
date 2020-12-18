62068b8c5db95751e09901c1f9c24594af069896
huilongyun <huilongyun@dearcc.cn>
2020-12-18 17:46:35 +0800

    update self determined layout

--- app/src/main/java/com/hly/learn/fragments/SelfDeterminedFragment.java
+++ app/src/main/java/com/hly/learn/fragments/SelfDeterminedFragment.java
@@ -6,10 +6,14 @@ import android.widget.Button;
 import com.hly.learn.R;
 import com.hly.learn.view.SelfDeterminedLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SelfDeterminedFragment extends BaseFragment {
 
     private SelfDeterminedLayout mSelfDeterminedLayout;
     private Button mUpdateTxt;
+    private List<String> mHotWordList = new ArrayList<>();
 
     @Override
     public int getLayoutId() {
@@ -19,20 +23,52 @@ public class SelfDeterminedFragment extends BaseFragment {
     @Override
     public void initData(View view) {
         mSelfDeterminedLayout = view.findViewById(R.id.selfDetermine);
-        mSelfDeterminedLayout.addView(2);
+        initHotWord(1);
+        mSelfDeterminedLayout.addView(mHotWordList);
         mUpdateTxt = view.findViewById(R.id.update);
         mUpdateTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mUpdateTxt.getText().equals("增加")) {
-                    mSelfDeterminedLayout.addView(4);
-                    mUpdateTxt.setText("减少");
+                if (mUpdateTxt.getText().equals("切换")) {
+                    initHotWord(0);
+                    mSelfDeterminedLayout.addView(mHotWordList);
+                    mUpdateTxt.setText("切回");
                 } else {
-                    mSelfDeterminedLayout.addView(2);
-                    mUpdateTxt.setText("增加");
+                    initHotWord(1);
+                    mSelfDeterminedLayout.addView(mHotWordList);
+                    mUpdateTxt.setText("切换");
                 }
                 mSelfDeterminedLayout.invalidate();
             }
         });
+    }
+
+    private void initHotWord(int num) {
+        if (num == 1) {
+            mHotWordList.clear();
+            mHotWordList.add("2020值得记忆");
+            mHotWordList.add("侄子侄女可继承叔伯遗产");
+            mHotWordList.add("素媛案罪犯出狱后被限制饮酒");
+            mHotWordList.add("欧阳娜娜 陈飞宇");
+            mHotWordList.add("灯芯绒穿搭");
+            mHotWordList.add("父亲回应未给女儿打狂犬疫苗原因");
+            mHotWordList.add("柳州袋装螺蛳粉产销超百亿元");
+            mHotWordList.add("魏晨再唱成全画面回放王鸥");
+            mHotWordList.add("外交部回应美或将80家中企列入黑名单");
+            mHotWordList.add("舒克和贝塔慎重结婚的原因");
+        } else {
+            mHotWordList.clear();
+            mHotWordList.add("舒克和贝塔慎重结婚的原因");
+            mHotWordList.add("热依扎3天瘦了7斤");
+            mHotWordList.add("刘昊然 我是唱歌不跑调的刘顶流");
+            mHotWordList.add("威廉王子最新全家福");
+            mHotWordList.add("郫都区新增确诊病例所在小区升为中风险");
+            mHotWordList.add("明星大侦探");
+            mHotWordList.add("美团杀熟门当事人回应");
+            mHotWordList.add("江苏一公园出现手机充电座椅");
+            mHotWordList.add("周深唱了明侦所有恐怖童谣");
+            mHotWordList.add("郭炜炜");
+            mHotWordList.add("新冠疫苗");
+        }
     }
 }
--- app/src/main/java/com/hly/learn/view/SelfDeterminedLayout.java
+++ app/src/main/java/com/hly/learn/view/SelfDeterminedLayout.java
@@ -2,9 +2,10 @@ package com.hly.learn.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.hly.learn.R;
 
@@ -13,8 +14,17 @@ import java.util.List;
 
 public class SelfDeterminedLayout extends RelativeLayout {
 
+    private static final int CONTENT_VIEW_TOP = 100;//第一行内容的top
+    private static final int LINE_HEIGHT = 64;//行高
+    private static final int ITEM_GAPS = 0;
+    private static final int TOTAL_WIDTH = 960;
+    private final List<View> mListView0 = new ArrayList<>();
+    private final List<View> mListView1 = new ArrayList<>();
+    private final List<View> mListView2 = new ArrayList<>();
+    private final List<View> mListView3 = new ArrayList<>();
+    private final List<View> mListView4 = new ArrayList<>();
+    private final List<View> mListView5 = new ArrayList<>();
     private Context mContext;
-    private List<View> mViews = new ArrayList<>();
 
     public SelfDeterminedLayout(Context context) {
         super(context);
@@ -37,80 +47,169 @@ public class SelfDeterminedLayout extends RelativeLayout {
         mContext = context;
     }
 
-    public void addView(int num) {
-        removeAllViews();
-        ImageView imageView1 = new ImageView(mContext);
-        imageView1.setImageResource(R.drawable.ic_chuancai);
-        imageView1.setScaleType(ImageView.ScaleType.FIT_XY);
-        addView(imageView1);
-        ImageView imageView2 = new ImageView(mContext);
-        imageView2.setImageResource(R.drawable.ic_lucai);
-        imageView2.setScaleType(ImageView.ScaleType.FIT_XY);
-        addView(imageView2);
-        if (num == 4) {
-            ImageView imageView3 = new ImageView(mContext);
-            imageView3.setImageResource(R.drawable.ic_xiangcai);
-            imageView3.setScaleType(ImageView.ScaleType.FIT_XY);
-            addView(imageView3);
-            ImageView imageView4 = new ImageView(mContext);
-            imageView4.setImageResource(R.drawable.ic_huicai);
-            imageView4.setScaleType(ImageView.ScaleType.FIT_XY);
-            addView(imageView4);
+    public void addView(List<String> list) {
+        if (list == null || list.size() == 0) {
+            setVisibility(View.GONE);
+            return;
         }
+        removeAllViews();
+        addHeaderView();
+        for (String text : list) {
+            TextView textView = new TextView(mContext);
+            textView.setPadding(30, 18, 30, 18);
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, 36);
+            textView.setIncludeFontPadding(false);
+            textView.setText(text);
+            textView.setTextColor(mContext.getColorStateList(R.color.colorPrimary));
+            addView(textView);
+        }
+    }
+
+    private void addHeaderView() {
+        TextView textView = new TextView(mContext);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, 48);
+        textView.setIncludeFontPadding(false);
+        textView.setText("热词搜索");
+        textView.setTextColor(mContext.getColor(R.color.color_red));
+        addView(textView);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int count = getChildCount();
-        mViews.clear();
-        for (int i = 0; i < count; i++) {
-            View view = getChildAt(i);
-            mViews.add(view);
-        }
         int width;
-        if (count == 2) {
-            width = 1200;
-        } else {
-            width = 1000;
+        mListView0.clear();
+        mListView1.clear();
+        mListView2.clear();
+        mListView3.clear();
+        mListView4.clear();
+        int index = 0;
+        int mTotalWidth;
+        if (1 < count) {
+            mListView0.add(getChildAt(0));
+            mTotalWidth = 0;
+            for (int i = 1; i < count; i++) {
+                View view = getChildAt(i);
+                width = view.getMeasuredWidth() + ITEM_GAPS;
+                mTotalWidth += width;
+                if (mTotalWidth < TOTAL_WIDTH) {
+                    mListView1.add(view);
+                } else {
+                    index = i;
+                    break;
+                }
+            }
         }
-        setMeasuredDimension(getMeasuredWidth(), width);
+        if (mListView1.size() + 1 < count) {
+            mTotalWidth = 0;
+            for (int i = index; i < count; i++) {
+                View view = getChildAt(i);
+                width = view.getMeasuredWidth() + ITEM_GAPS;
+                mTotalWidth += width;
+                if (mTotalWidth < TOTAL_WIDTH) {
+                    mListView2.add(view);
+                } else {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        if (mListView1.size() + mListView2.size() + 1 < count) {
+            mTotalWidth = 0;
+            for (int i = index; i < count; i++) {
+                View view = getChildAt(i);
+                width = view.getMeasuredWidth() + ITEM_GAPS;
+                mTotalWidth += width;
+                if (mTotalWidth < TOTAL_WIDTH) {
+                    mListView3.add(view);
+                } else {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        if (mListView1.size() + mListView2.size() + mListView3.size() + 1 < count) {
+            mTotalWidth = 0;
+            for (int i = index; i < count; i++) {
+                View view = getChildAt(i);
+                width = view.getMeasuredWidth() + ITEM_GAPS;
+                mTotalWidth += width;
+                if (mTotalWidth < TOTAL_WIDTH) {
+                    mListView4.add(view);
+                } else {
+                    mListView5.add(view);
+                }
+            }
+        }
+        //根据可展示的内容量调整View的高度
+        int measureWidth = getMeasuredWidth();
+        if (mListView4.size() > 0) {
+            setMeasuredDimension(measureWidth, 364);
+        } else if (mListView3.size() > 0) {
+            setMeasuredDimension(measureWidth, 300);
+        } else if (mListView2.size() > 0) {
+            setMeasuredDimension(measureWidth, 236);
+        } else if (mListView1.size() > 0) {
+            setMeasuredDimension(measureWidth, 172);
+        }
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
-        int left;
-        int top;
-        int width;
-        int height;
-        int count = mViews.size();
-        left = 240;
-        top = 50;
-        if (count == 2) {
-            width = 600;
-            height = 480;
-        } else {
-            width = 400;
-            height = 450;
+        int left = 30;
+        int top = 18;
+        int size = mListView0.size();
+        for (int i = 0; i < size; i++) {
+            View view = mListView0.get(i);
+            int width = view.getMeasuredWidth();
+            view.layout(left, top, left + width, top + view.getMeasuredHeight());
+            left += width + ITEM_GAPS;
         }
-        for (int i = 0; i< count; i++) {
-            View view = mViews.get(i);
-            if (count == 4) {
-                if (i == 0 || i == 2) {
-                    left = 90;
-                } else {
-                    left = 600;
-                }
-                if (i == 2) {
-                    top += height + 50;
-                }
-            }
-            view.layout(left, top, left + width, top + height);
-            if (count == 2) {
-                top += height + 50;
-            }
+        left = 0;
+        size = mListView1.size();
+        for (int i = 0; i < size; i++) {
+            View view = mListView1.get(i);
+            int width = view.getMeasuredWidth();
+            int height = view.getMeasuredHeight();
+            view.layout(left, CONTENT_VIEW_TOP, left + width, CONTENT_VIEW_TOP + height);
+            left += width + ITEM_GAPS;
+        }
+        left = 0;
+        size = mListView2.size();
+        for (int i = 0; i < size; i++) {
+            View view = mListView2.get(i);
+            int width = view.getMeasuredWidth();
+            int height = view.getMeasuredHeight();
+            view.layout(left, CONTENT_VIEW_TOP + LINE_HEIGHT, left + width, CONTENT_VIEW_TOP + LINE_HEIGHT + height);
+            left += width + ITEM_GAPS;
+        }
+        left = 0;
+        size = mListView3.size();
+        for (int i = 0; i < size; i++) {
+            View view = mListView3.get(i);
+            int width = view.getMeasuredWidth();
+            int height = view.getMeasuredHeight();
+            view.layout(left, CONTENT_VIEW_TOP + LINE_HEIGHT * 2, left + width, CONTENT_VIEW_TOP + LINE_HEIGHT * 2 + height);
+            left += width + ITEM_GAPS;
+        }
+        left = 0;
+        size = mListView4.size();
+        for (int i = 0; i < size; i++) {
+            View view = mListView4.get(i);
+            int width = view.getMeasuredWidth();
+            int height = view.getMeasuredHeight();
+            view.layout(left, CONTENT_VIEW_TOP + LINE_HEIGHT * 3, left + width, CONTENT_VIEW_TOP + LINE_HEIGHT * 3 + height);
+            left += width + ITEM_GAPS;
+        }
+        //剩余的不显示了
+        size = mListView5.size();
+        for (int i = 0; i < size; i++) {
+            View view = mListView5.get(i);
+            //两种方法都可以实现
+            //view.setVisibility(GONE);
+            removeView(view);
         }
     }
-
 }
