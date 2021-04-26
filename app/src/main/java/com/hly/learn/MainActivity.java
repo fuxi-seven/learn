2b51d130be68b25495530b2d84edfb5d9787b2c2
huilongyun <huilongyun@enovatemotors.com>
2021-04-26 17:10:12 +0800

    update main layout issue

--- app/src/main/java/com/hly/learn/MainActivity.java
+++ app/src/main/java/com/hly/learn/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
-import com.hly.learn.hook.HookUtils;
 import com.hly.learn.view.FragmentsAdapter;
 
 public class MainActivity extends AppCompatActivity {
