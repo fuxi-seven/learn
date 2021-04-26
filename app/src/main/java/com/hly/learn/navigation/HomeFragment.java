88677be17223d0c2c53e02f1d38290434ad9eef4
huilongyun <huilongyun@enovatemotors.com>
2021-04-26 14:39:20 +0800

    add jetpack navigation

---
+++ app/src/main/java/com/hly/learn/navigation/HomeFragment.java
@@ -0,0 +1,18 @@
+package com.hly.learn.navigation;
+
+import android.view.View;
+
+import com.hly.learn.R;
+import com.hly.learn.fragments.BaseFragment;
+
+public class HomeFragment extends BaseFragment {
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_home;
+    }
+
+    @Override
+    public void initData(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/hly/learn/navigation/NotificationFragment.java
@@ -0,0 +1,18 @@
+package com.hly.learn.navigation;
+
+import android.view.View;
+
+import com.hly.learn.R;
+import com.hly.learn.fragments.BaseFragment;
+
+public class NotificationFragment extends BaseFragment {
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_notifications;
+    }
+
+    @Override
+    public void initData(View view) {
+
+    }
+}
